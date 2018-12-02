6207c4c91f10ca0eb746db7e77d0e81ba99195f1
TheAzou <pika.azou@gmail.com>
2018-12-02 14:55:02 +0100

    Ajout du menu et du formulaire de céation de match

---
+++ Home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+</head>
+
+<body>
+		<?php 
+		include('menu.html');
+		 ?>
+
+		
+		<div class="content">
+
+			<div class="date"> 
+				<img src="icone/calendrier">
+				<p><?php 
+					$today = date("d.m.y"); 
+					echo "$today"; 
+				?></p>
+			</div>
+
+
+			<div class="match-prevu">
+				<p> Prochain match prévu </p>
+			</div>
+
+			<div class="redirection-accueil">
+				<a href="joueurs.html"><button class="boutton-accueil">Visualiser les joueurs</button></a>
+				<a href="matchs.html"><button class="boutton-accueil">Visualiser les matchs</button></a>
+				<a href="statistiques.html"><button class="boutton-accueil">Statistiques des joueurs</button></a>
+			</div>
+		</div>
+
+
+
+</body>
+</html>
---
+++ creation-match.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Création de match</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+</head>
+
+<body>
+
+	<?php 
+		include('menu.html');
+	?>
+
+	<div class="content">
+			
+			<div class="date"> 
+				<img src="icone/calendrier">
+				<p><?php
+					 $today = date("d.m.y"); 
+					echo "$today"; 
+				?></p>
+			</div>
+
+			<div class="formulaire">
+				<p>Saisissez les informations relatives au nouveau match.</p>
+
+
+				<form action="matchs.php" method="post">
+					<input name="equipe" type="text" placeholder="Nom de l'équipe adverse" required> <br />
+
+					<p>Date et heure du match</p>
+					 <input name="date" type="date" placeholder="JJ/MM/AAAA" required> 
+
+					<input name="time" type="time" required> <br />
+
+					<p> Lieu de rencontre </p>
+					<input type="radio" name="domicile" value=1 checked>
+ 					 <label>Domicile</label>  <br />				
+	 				<input type="radio" name="extérieur" value=2> 
+	 				 <label>Extérieur</label>  <br />
+ 					
+
+					<input type="submit" value="Valider">
+				</form>
+
+			</div>
+	</div>
+
+</body>
+</html>
