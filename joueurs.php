6a41d8b0f552b50ddd0588dc073efcaecfd9a41b
Ines Louahadj <lhn3042a@r-info-gren-l20.iut.iut-tlse3.fr>
2018-12-05 15:20:58 +0100

    Pas important

---
+++ joueurs.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Liste des joueurs</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+</head>
+
+<body>
+
+	<?php 
+		require('menu.php');
+	?>
+
+	<div class="content">
+			
+
+			<div class="affichage_joueurs">
+				
+			</div>
+	</div>
+
+</body>
+</html>
